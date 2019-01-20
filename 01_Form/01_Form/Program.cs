749b3a62de97a996a9847ed1a67716cfb145c059
MuxaTim <muxatim@gmail.com>
2019-01-21 05:13:47 +0700

    Создал проект в VS 2017. В консоль выводится шапка (студент/курс/урок/ДЗ).

---
+++ 01_Form/01_Form/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Form
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Студент: Тимченко Михаил");
+            Console.WriteLine("Курс: C# Уровень 1");
+            Console.WriteLine("Урок: 1.Введение.Базовые типы данных.Консоль.Классы и методы.");
+            Console.Write("Домашнее задание №1:\nНаписать программу «Анкета».\nПоследовательно задаются вопросы (имя, фамилия, возраст, рост, вес).\nВ результате вся информация выводится в одну строчку.");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
