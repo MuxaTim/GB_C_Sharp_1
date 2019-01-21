36c32da558e783c8b37e39b3a6760650daff2d36
MuxaTim <muxatim@gmail.com>
2019-01-21 13:12:34 +0700

    Выполнил ДЗ №6 - Создать класс с методами, которые могут пригодиться в вашей учебе (Print, Pause)

---
+++ 06_MyClass/06_MyClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_MyClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void Print(string str)
+        {
+            Console.WriteLine(str);          
+        }
+
+        static void Print(string msg, int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(msg);
+        }
+
+        static void Pause()                    
+        {
+            Console.ReadKey();
+        }
+
+
+        static void Pause(string str)
+        {
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 06_MyClass/06_MyClass/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("06_MyClass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("06_MyClass")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("eb2efaa6-d0cb-45dc-9b25-985e0017cf31")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
