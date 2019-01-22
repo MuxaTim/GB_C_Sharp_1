c65c7a149b8298b1c5d5b434adf7b930875915cf
MuxaTim <muxatim@gmail.com>
2019-01-23 04:12:44 +0700

    Выполнил Домашнее задание: 1. Написать метод, возвращающий минимальное из трех чисел.

--- Lesson-02/01_Minimum/01_Minimum/Program.cs
+++ Lesson-02/01_Minimum/01_Minimum/Program.cs
@@ -16,14 +16,27 @@ namespace _01_Minimum
             MyClass.TextAbout(lesson, homework); //воспользуюсь своей библиотекой для создания шапки
 
             //запросим у пользователя 3 числа
-            int a1 = MyClass.InputInt("Введите число 1:");
+            float a1 = MyClass.InputFloat("Введите число 1:");
             float a2 = MyClass.InputFloat("Введите число 2:");
+            float a3 = MyClass.InputFloat("Введите число 3:");
 
-            Console.WriteLine(a1);
-            Console.WriteLine(a2);
-
+            float min = Minimum(a1, a2, a3);
+            Console.WriteLine($"Вы ввели числа {a1}, {a2}, {a3}.\nМинимальное из этих чисел: {min}");
 
             Console.ReadLine();
+        }
+
+        //метод Minimum возвращает минимальное из 3 переданных ему чисел
+        static float Minimum(float a1, float a2, float a3)
+        {
+            float min = a1;
+            if (min>a2) {
+                min = a2;
+            }
+            else if (min > a3) {
+                min = a3;
+            }
+            return min;
         }
     }
 }
