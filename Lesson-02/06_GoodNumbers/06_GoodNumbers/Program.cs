66a1db483a34bfa02707a384371720b25a33b2fe
MuxaTim <muxatim@gmail.com>
2019-01-23 12:09:39 +0700

    ДЗ 6 - написал метод, подсчитывающий сумму цифр в числе

---
+++ Lesson-02/06_GoodNumbers/06_GoodNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using MyLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_GoodNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string lesson = "Урок 2. Управляющие конструкции";
+            string homework = "Домашнее задание: 6. *Написать программу подсчета количества «Хороших» чисел в диапазоне от 1 до 1 000 000 000. Хорошим называется число, которое делится на сумму своих цифр. Реализовать подсчет времени выполнения программы, используя структуру DateTime.";
+            MyClass.TextAbout(lesson, homework); //воспользуюсь своей библиотекой для создания шапки
+
+            int number = MyClass.InputInt("");
+            int a = NumbersSum(number);
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+
+        //метод NumbersSum подсчитывает сумму цифр в числе
+        static int NumbersSum (int number)
+        {
+            int count = 0;
+            while (number != 0)
+            {
+                count = count + number % 10;
+
+                number = number / 10;
+            }
+            return count;
+        }
+    }
+}
