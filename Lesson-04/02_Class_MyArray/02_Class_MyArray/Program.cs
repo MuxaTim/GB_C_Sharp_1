374b1052b8f214d9779a970b67e14d2dfc7da303
MuxaTim <muxatim@gmail.com>
2019-01-31 10:38:51 +0700

    ДЗ 2: - В библиотеку MyLibrary переписал класс MyArray из методички; - В примере создал экземпляр класса MyArray, и вывел его в консоль.

---
+++ Lesson-04/02_Class_MyArray/02_Class_MyArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary;
+
+namespace _02_Class_MyArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Создаём экземпляр класса MyArray
+            MyArray a = new MyArray(10, 5);
+
+            Console.WriteLine(a);
+
+
+            Console.ReadLine();
+        }
+    }
+}
