04e902bb2bf0ab400da53bd9da93a48283ee0632
MuxaTim <muxatim@gmail.com>
2019-01-31 10:15:35 +0700

    ДЗ 1: - Диапазон рандома задается через переменные rndMin и rndMax; - В цикле сразу делаем проверку пары i-го и (i-1)-го элементов; - Выводим в консоль значение счетчика.

--- Lesson-04/01_Array/01_Array/Program.cs
+++ Lesson-04/01_Array/01_Array/Program.cs
@@ -17,13 +17,32 @@ namespace _01_Array
 
             int[] array = new int[20];
             Random r = new Random();
+            int rndMin = -10000;
+            int rndMax = 10001;
+            int a = 0; //счетчик пар, отвечающих условию
 
-            for (int i = 0; i < array.Length; i++)
+            Console.WriteLine("Элементы массива:");
+
+            // Заранее зададим значение 0-го элемента
+            array[0] = r.Next(rndMin, rndMax);
+            Console.Write(array[0]);
+
+            // Далее в цикле задаём значения элементов, начиная с 1-го
+            // И проверяем i-ый и (i-1)-ый элементы
+            for (int i = 1; i < array.Length; i++) 
             {
-                array[i] = r.Next(-10000, 10001);
-                Console.WriteLine(array[i]);
+                array[i] = r.Next(rndMin, rndMax);
+                Console.Write(", " + array[i]);
+
+                // Проверяем пару элементов
+                // Если хотябы один из них делится на 3 без остатка, то увеличиваем счетчик
+                if (array[i-1] % 3 == 0 || array[i] % 3 == 0)
+                {
+                    a++;
+                }
             }
-               
+
+            Console.WriteLine("\nКоличество пар элементов массива, отвечающих условию: " + a);  
 
             Console.ReadLine();
         }
